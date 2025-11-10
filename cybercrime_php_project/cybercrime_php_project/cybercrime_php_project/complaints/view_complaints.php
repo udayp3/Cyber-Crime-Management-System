ea7878f8fc46ed653a08b24deaee798e4d341a1d
uday p <udayapoojar3@gmail.com>
2025-11-10 20:25:32 +0530

    Delete cybercrime_php_project/cybercrime_php_project/cybercrime_php_project directory

--- cybercrime_php_project/cybercrime_php_project/cybercrime_php_project/complaints/view_complaints.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../db.php';
-$res = $conn->query("SELECT * FROM complaints");
-while($row = $res->fetch_assoc()) {
-    echo "ID: " . $row['id'] . " | " . $row['title'] . " | " . $row['status'] . "<br>";
-}
-?>
